3f46af822a1561cfdaae677103c8d7e001133ed4
Anthony Ringoet <anthonyringoet@gmail.com>
2014-04-04 12:06:36 +0200

    server test - plugins loaded

--- test/server.js
+++ test/server.js
@@ -4,8 +4,24 @@ var server = require('../server');
 var expect = Lab.expect;
 var describe = Lab.experiment;
 var it = Lab.test;
+var plugins = Object.keys(server.pack.list);
 
 describe('Server', function(){
+
+  it('should have Homepage plugin', function (done) {
+    expect(plugins.indexOf('Homepage')).not.to.equal(-1);
+    done();
+  });
+
+  it('should have Coffee plugin', function (done) {
+    expect(plugins.indexOf('Coffee')).not.to.equal(-1);
+    done();
+  });
+
+  it('should have Coffeemaker plugin', function (done) {
+    expect(plugins.indexOf('Coffeemaker')).not.to.equal(-1);
+    done();
+  });
 
   it('calls /', function(done){
     server.inject({ method: 'GET', url: '/' }, function(res){
